3f85f5412ba622934ae870384595358d31d4c48a
Viktor <volgofant@mail.ru>
2019-04-16 09:35:18 +0300

    first timer

--- yoga - tabs 7/js/script.js
+++ yoga - tabs 7/js/script.js
@@ -35,4 +35,44 @@ window.addEventListener("DOMContentLoaded", function() {
             }
         }
     });
+
+
+    let deadLine = "2019-04-25";
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000)%60),
+            minutes = Math.floor((t/1000/60)%60),
+            hours = Math.floor((t/(1000*60*60)));
+
+            return {
+                "total": t,
+                "hours": hours,
+                "minutes": minutes,
+                "seconds": seconds
+            }
+    }
+
+
+    function setCloack(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector("hourse"),
+            minutes = timer.querySelector("minutes"),
+            seconds = timer.querySelector("seconds"),
+            timeInterval = setInterval(updateCloack, 1000);
+
+            function updateCloack () {
+                let t = getTimeRemaining(endTime);
+                hours.textContent = t.hours;
+                minutes.textContent = t.minutes;
+                seconds.textContent = t.seconds;
+
+                if (t.total <= 0) {
+                    clearInterval(timeInterval);
+                }
+            }
+
+    }
+
+    setCloak("timer", deadLine)
 });
